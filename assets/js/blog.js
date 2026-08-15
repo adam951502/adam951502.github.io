13d707523a1f81fa68ce94b9e75cff380430a977
Adam Tang <52702712+adam951502@users.noreply.github.com>
2026-08-15 21:37:08 +0800

    feat: add validated Blog route module

---
+++ assets/js/blog.js
@@ -0,0 +1,32 @@
+import { createI18nController } from "./i18n.js";
+import { createMobileMenuController } from "./mobile-menu.js";
+import { createThemeController } from "./theme.js";
+
+const themeToggle = document.querySelector(".theme-toggle");
+const themeToggleLabel = document.querySelector(".theme-toggle-label");
+let i18nController;
+
+const themeController = createThemeController({
+  root: document.documentElement,
+  themeToggle,
+  themeToggleLabel,
+  getCurrentLang: () => i18nController?.getCurrentLang() || "en"
+});
+
+const mobileMenuController = createMobileMenuController({
+  closeLanguageMenu: () => i18nController?.closeMenu()
+});
+
+i18nController = createI18nController({
+  i18nBasePath: "../assets/i18n",
+  pageTitleKey: "blog.page.title",
+  onApply: async ({ translate }) => {
+    themeController.setLabel(translate);
+  }
+});
+
+mobileMenuController.bind();
+themeController.bind();
+i18nController.bind();
+themeController.applyInitial();
+i18nController.applyTranslations(i18nController.getCurrentLang());
